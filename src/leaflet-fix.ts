1b6e3ddb53ef112679c1383a405cebd08e5aaab4
fakfarrukh <fak_farrukh@yahoo.com>
2025-11-22 22:14:47 +0500

    environemnt issue

--- src/leaflet-fix.ts
+++ src/leaflet-fix.ts
@@ -1,16 +1,16 @@
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/leaflet.css";
 
-interface IconDefaultWithPrivate extends L.Icon.Default {
-  _getIconUrl?: any;
-}
+// Fix default icon
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 
-delete (L.Icon.Default.prototype as IconDefaultWithPrivate)._getIconUrl;
-
-(L.Icon.Default as any).mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
 });
 
 export default L;
