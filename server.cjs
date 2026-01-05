061279c0f5d20bcd3c7dc0d323dbf33c9e1a4822
fakfarrukh <fak_farrukh@yahoo.com>
2026-01-05 17:59:32 +0500

    issues fixed

--- server.cjs
+++ server.cjs
@@ -1,17 +1,7 @@
 const app = require('./backend/app.cjs');
-const path = require('path');
-const express = require('express');
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all using RegExp (REQUIRED)
-app.get(/^(?!\/api).*/, (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
